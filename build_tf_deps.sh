14957a2a6b6ca7299467eca28a79f93fec02afc9
Robert Grzelka <robert.grzelka@outlook.com>
2018-11-28 21:57:06 +0100

    .

--- build_tf_deps.sh
+++ build_tf_deps.sh
@@ -10,27 +10,30 @@ set -e
 ###################################################################################################
 
 cd ${HOME}/bin
-git clone https://github.com/jemalloc/jemalloc || true
-cd jemalloc
-wget -nc https://github.com/jemalloc/jemalloc/releases/download/5.1.0/jemalloc-5.1.0.tar.bz2
-tar -xkjf jemalloc-5.1.0.tar.bz2 || true 
-cd jemalloc-5.1.0
-./configure --prefix=$HOME/jemalloc/jemalloc-5.1.0/build
-make
-make all install
-
+if ! cd jemalloc; then
+    git clone https://github.com/jemalloc/jemalloc
+    cd jemalloc
+    wget -nc https://github.com/jemalloc/jemalloc/releases/download/5.1.0/jemalloc-5.1.0.tar.bz2
+    tar -xkjf jemalloc-5.1.0.tar.bz2 || true 
+    cd jemalloc-5.1.0
+    ./configure --prefix=$HOME/jemalloc/jemalloc-5.1.0/build
+    make
+    make all install
+fi
 
 ###################################################################################################
 ###################################################################################################
 
 cd ${HOME}/bin
+if ! cd computecpp; then
 mkdir -p computecpp  || true
 cd computecpp
 wget -nc https://developer.codeplay.com/download/?key=1fad850f6661cd4364b81a14e1a4387c
 mv "index.html?key=1fad850f6661cd4364b81a14e1a4387c" ComputeCpp-CE-1.0.2-Ubuntu.16.04-64bit.tar.gz
 chmod 777 *
 tar -xzkf ComputeCpp*tar.gz || true 
-mv Com*64 ComputeCpp-CE-1.0.2
+mv ComputeCpp-CE-1.0.2-Ubuntu.16.04-64bit ComputeCpp-CE-1.0.2
+fi
 
 ###################################################################################################
 ###################################################################################################
@@ -39,19 +42,19 @@ cd ${HOME}/bin
 if ! cd nccl; then
 git clone https://github.com/NVIDIA/nccl
 cd nccl
+make -j src.build
 fi
 
-make -j src.build
 
 cd ${HOME}/bin
 if ! cd nccl-tests; then
 git clone https://github.com/NVIDIA/nccl-tests.git
 cd nccl-tests
+make
+./build/all_reduce_perf -b 8 -e 256M -f 2 -g 1
 fi
 
-make
 
-./build/all_reduce_perf -b 8 -e 256M -f 2 -g 1
 
 
 
